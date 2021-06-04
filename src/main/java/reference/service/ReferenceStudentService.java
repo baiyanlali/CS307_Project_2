1068dd77da9dfd0630507db1eeda355ec944d02c
Cody-Banks7 <2960236602@qq.com>
2021-06-04 17:25:58 +0800

    Update ReferenceStudentService.java

--- src/main/java/reference/service/ReferenceStudentService.java
+++ src/main/java/reference/service/ReferenceStudentService.java
@@ -1,5 +1,6 @@
 package reference.service;
 
+import cn.edu.sustech.cs307.database.SQLDataSource;
 import cn.edu.sustech.cs307.dto.Course;
 import cn.edu.sustech.cs307.dto.CourseSearchEntry;
 import cn.edu.sustech.cs307.dto.CourseTable;
@@ -8,7 +9,7 @@ import cn.edu.sustech.cs307.dto.grade.Grade;
 import cn.edu.sustech.cs307.service.StudentService;
 
 import javax.annotation.Nullable;
-import java.sql.Date;
+import java.sql.*;
 import java.time.DayOfWeek;
 import java.util.List;
 import java.util.Map;
@@ -16,27 +17,70 @@ import java.util.Map;
 public class ReferenceStudentService implements StudentService {
     @Override
     public void addStudent(int userId, int majorId, String firstName, String lastName, Date enrolledDate) {
-
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement("call (?,?,?) ")) {
+            stmt.setInt(1, userId);
+            stmt.setString(2, firstName);
+            stmt.setString(3,lastName);
+            stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<CourseSearchEntry> searchCourse(int studentId, int semesterId, @Nullable String searchCid, @Nullable String searchName, @Nullable String searchInstructor, @Nullable DayOfWeek searchDayOfWeek, @Nullable Short searchClassTime, @Nullable List<String> searchClassLocations, CourseType searchCourseType, boolean ignoreFull, boolean ignoreConflict, boolean ignorePassed, boolean ignoreMissingPrerequisites, int pageSize, int pageIndex) {
-        return null;
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement("select searchCourse(?,?,?,?) ")) {
+            stmt.setInt(1, studentId);
+            stmt.setInt(2, semesterId);
+            stmt.setString(3,);
+            stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public EnrollResult enrollCourse(int studentId, int sectionId) {
-        return null;
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement("call enrollCourse(?,?) ")) {
+            stmt.setInt(1, studentId);
+            stmt.setInt(2, sectionId);
+            stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void dropCourse(int studentId, int sectionId) throws IllegalStateException {
-
+    public void dropCourse(int studentId, int sectionId) {
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement("call drop_course(?, ?)")) {
+            stmt.setInt(1, studentId);
+            stmt.setInt(2, sectionId);
+            stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void addEnrolledCourseWithGrade(int studentId, int sectionId, @Nullable Grade grade) {
+        try (Connection connection = SQLDataSource.getInstance().getSQLConnection();
+             PreparedStatement stmt = connection.prepareStatement("call addEnrolledCourseWithGrade(?, ?)")) {
+            stmt.setInt(1, studentId);
+            stmt.setInt(2, sectionId);
+            stmt.execute();
 
+            ResultSet rs=stmt.executeQuery();
+            while(rs.next()){
+                String name=rs.getString("name");
+                
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -51,6 +95,7 @@ public class ReferenceStudentService implements StudentService {
 
     @Override
     public CourseTable getCourseTable(int studentId, Date date) {
+
         return null;
     }
 
