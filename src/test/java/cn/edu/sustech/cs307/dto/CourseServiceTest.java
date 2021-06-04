8f46d04b625d02827c65b45c908a90a58801250a
MarsTSX <846030067@qq.com>
2021-06-04 22:02:39 +0800

    finish prerequisite and connection on it.

--- src/test/java/cn/edu/sustech/cs307/dto/CourseServiceTest.java
+++ src/test/java/cn/edu/sustech/cs307/dto/CourseServiceTest.java
@@ -1,5 +1,9 @@
 package cn.edu.sustech.cs307.dto;
 
+import cn.edu.sustech.cs307.dto.prerequisite.AndPrerequisite;
+import cn.edu.sustech.cs307.dto.prerequisite.CoursePrerequisite;
+import cn.edu.sustech.cs307.dto.prerequisite.OrPrerequisite;
+import cn.edu.sustech.cs307.dto.prerequisite.Prerequisite;
 import cn.edu.sustech.cs307.service.CourseService;
 import reference.service.ReferenceCourseService;
 
@@ -10,19 +14,31 @@ import java.util.List;
 
 public class CourseServiceTest {
     public static void main(String[] args) {
+        Prerequisite calculus = new OrPrerequisite(List.of(
+                new CoursePrerequisite("MA101A"),
+                new CoursePrerequisite("MA101B")
+        ));
+        Prerequisite algebra = new CoursePrerequisite("MA103A");
+        Prerequisite prerequisite = new AndPrerequisite(List.of(calculus, algebra));
 
         CourseService cs=new ReferenceCourseService();
-<<<<<<< HEAD
-        cs.removeCourse("MIAO101");
-        cs.addCourse("MIAO101","猫猫学导论",5,32, Course.CourseGrading.PASS_OR_FAIL,null);
-        int sec_id = cs.addCourseSection("MIAO101",1,"猫猫学期末学期~",20);
-        DayOfWeek dow=DayOfWeek.FRIDAY;
-        List<Short> weeklist=new ArrayList<>();
-        
+        cs.addCourse("MA101A", "高等数学上",4,64, Course.CourseGrading.HUNDRED_MARK_SCORE, null);
+        cs.addCourse("MA101B", "低等数学上", 4, 64, Course.CourseGrading.HUNDRED_MARK_SCORE, null);
+        cs.addCourse("MA103A", "线性代数", 4, 64, Course.CourseGrading.HUNDRED_MARK_SCORE, null);
+        cs.addCourse("MA102A", "超等数学", 4, 64, Course.CourseGrading.HUNDRED_MARK_SCORE, prerequisite);
+//        cs.removeCourse("MA102A");
+//        cs.removeCourse("MA101A");
+//        cs.removeCourse("MA101B");
+//        cs.removeCourse("MA103A");
+
+
 //        cs.addCourseSectionClass(sec_id,1);
-        Course ccc = cs.getCourseBySection(999);
-        System.out.println(ccc);
-=======
+//        Course ccc = cs.getCourseBySection(999);
+//        System.out.println(ccc);
+
+
+
+//=======
 //        cs.removeCourse("MIAO101");
 //        List<Course> cc =  cs.getAllCourses();
 //        System.out.println(cc);
@@ -32,7 +48,7 @@ public class CourseServiceTest {
 //        var csis=cs.getCourseSectionByClass(4);
 //        var csis=cs.getCourseSectionClasses(4);
 //        var csis=cs.getEnrolledStudentsInSemester("MARS101",1);
-        cs.removeCourse("MARS102");
+//        cs.removeCourse("MARS102");
 //        System.out.println(csis);
 //        cs.addCourse("MIAO101","猫猫学导论",5,32, Course.CourseGrading.PASS_OR_FAIL,null);
 //        int sec_id = cs.addCourseSection("MIAO101",1,"猫猫学期末学期~",20);
@@ -46,6 +62,6 @@ public class CourseServiceTest {
 //
 //        Course ccc = cs.getCourseBySection(999);
 //        System.out.println(ccc);
->>>>>>> c16a8af558ce1ab46bf5f10075e4e152000e1983
+//>>>>>>> c16a8af558ce1ab46bf5f10075e4e152000e1983
     }
 }
