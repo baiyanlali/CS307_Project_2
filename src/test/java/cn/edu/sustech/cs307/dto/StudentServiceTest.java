4bed4a55791467f971c01848df8d9405c7a92cc6
baiyanlali <zao44457660@126.com>
2021-06-12 10:42:52 +0800

    update some exception

--- src/test/java/cn/edu/sustech/cs307/dto/StudentServiceTest.java
+++ src/test/java/cn/edu/sustech/cs307/dto/StudentServiceTest.java
@@ -16,7 +16,7 @@ public class StudentServiceTest {
 //        var v1 = ss.getStudentMajor(11911311);
 //        var v1=ss.enrollCourse(11911311,1);
 //        var v2 = ss.getStudentMajor(11911311);
-        var v2 = ss.searchCourse(11717372,145,null,null,null,null,null,null, StudentService.CourseType.ALL,false,false,true,true,30,0);
+        var v2 = ss.searchCourse(11713068,147,null,null,null,null,null,null, StudentService.CourseType.ALL,true,false,true,true,10,5);
 //        var v2=ss.getCourseTable(11712973,Date.valueOf(LocalDate.ofEpochDay(17934)));
 //        System.out.println(LocalDate.ofEpochDay(17934));
         System.out.println(v2);
